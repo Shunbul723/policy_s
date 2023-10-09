fdb63874cebb5acaa8e8a4c3788de19d1ebcf1a9
Shunbul723 <143285228+Shunbul723@users.noreply.github.com>
2023-10-10 01:06:53 +0530

    new

---
+++ src/test/java/com/staragile/insurance/policy/PolicyApplicationTests.java
@@ -0,0 +1,17 @@
+package com.staragile.insurance.policy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Testing started");
+		System.out.println("Testing ok");
+		System.out.println("Testing ended");
+		
+	}
+
+}
